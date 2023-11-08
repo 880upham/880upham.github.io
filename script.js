42bf99a10fd679be8f661be155d9f67847c2ecad
880upham <150109077+880upham@users.noreply.github.com>
2023-11-08 07:00:42 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
     const textContainer = document.getElementById("text-container");
 
-        // After 1 second, fade in the initial text
-        textContainer.textContent = "Are You Ready...";
-        setTimeout(function () {
-            textContainer.style.opacity = "1";
-        }, 1000);
-
-        // After 5 seconds, change the text to something else
-        setTimeout(function () {
-            textContainer.style.opacity = "0"; // Fade out the text
-            setTimeout(function () {
-                textContainer.textContent = "To Get Fucked?"; // Change the text content
-                textContainer.style.opacity = "1"; // Fade in the new text
-            }, 1000); // Adjust the timing as needed
-        }, 2000);
-
     setTimeout(function () {
         const whitePoint = document.getElementById("white-point");
         whitePoint.style.transform = "scale(200)";
 
-        // After animation, remove the black screen
+        // After 1 second, fade in the initial text
+        textContainer.textContent = "Are You Ready...";
+        textContainer.style.opacity = "1";
+
+        // After 3 seconds, start fading out the initial text
         setTimeout(function () {
-            document.querySelector(".black-screen").style.display = "none";
-        }, 7000); // Adjust the timing as needed
-    }, 5000);
+            textContainer.style.opacity = "0";
+
+            // After 1 second, change the text to something else
+            setTimeout(function () {
+                textContainer.textContent = "To Get Fucked?"; // Change the text content
+
+                // After 1 second, fade in the new text
+                setTimeout(function () {
+                    textContainer.style.opacity = "1"; // Fade in the new text
+                }, 1000); // Adjust the timing as needed
+            }, 1000); // Adjust the timing as needed
+        }, 3000); // Adjust the timing as needed
+    }, 1000);
 });
