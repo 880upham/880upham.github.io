6d8b176d4ea18cabc6d495c007f03e273babab77
880upham <150109077+880upham@users.noreply.github.com>
2023-11-09 20:13:33 -0800

    code reformat

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function() {
+    const textContainer = document.getElementById("text-container");
+    const backgroundContainer = document.getElementById('background-container');
+
+    backgroundContainer.classList.toggle('image1');
+    await sleep(1)
+    textContainer.textContent = "Are You Ready...";
+    textContainer.style.opacity = "1";
+    await sleep(2)
+    await fadeOutAndIn()
+    textContainer.textContent = "To Get Fucked?"; // Change the text content
+    await sleep(3)
+    await fadeOutAndIn()
+    textContainer.textContent = "Get Fucked"; // Change the text content
+    backgroundContainer.classList.toggle('image2');
+});
+
+
+function sleep(d) {
+    return new Promise(resolve => setTimeout(resolve, d * 1000));
+}
+async function fadeOutAndIn() {
     const textContainer = document.getElementById("text-container");
 
-    setTimeout(function () {
-        const whitePoint = document.getElementById("white-point");
-        whitePoint.style.transform = "scale(200)";
-
-        // After 1 second, fade in the initial text
-        textContainer.textContent = "Are You Ready...";
-        textContainer.style.opacity = "1";
-
-        // After 3 seconds, start fading out the initial text
-        setTimeout(function () {
-            textContainer.style.opacity = "0";
-
-            // After 1 second, change the text to something else
-            setTimeout(function () {
-                textContainer.textContent = "To Get Fucked?"; // Change the text content
-
-                // After 1 second, fade in the new text
-                setTimeout(function () {
-                    textContainer.style.opacity = "1"; // Fade in the new text
-                }, 1000); // Adjust the timing as needed
-            }, 1000); // Adjust the timing as needed
-        }, 3000); // Adjust the timing as needed
-    }, 1000);
-});
+    await sleep(1)
+    textContainer.style.opacity = "0";
+    await sleep(1)
+    textContainer.style.opacity = "1";
+}
