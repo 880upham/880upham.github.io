37224fc62bbf80655c12fb5f011f90002e15238d
Thomas Rich <tnrich@teselagen.com>
2023-11-12 20:23:33 -0800

    wip building

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        chat: resolve(__dirname, 'chat/index.html'),
+        chat: resolve(__dirname, 'chat.html'),
       },
     },
   },
