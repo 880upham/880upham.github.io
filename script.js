d608d3362f8cd48ed96394dc799c30b379978eda
Thomas Rich <tnrich@teselagen.com>
2023-11-13 18:33:22 -0800

    zoink

--- script.js
+++ script.js
@@ -65,8 +65,8 @@ document.addEventListener("DOMContentLoaded", async function () {
 });
 
 function sleep(d) {
-  const fast = true;
-  // const fast = false;
+  // const fast = true;
+  const fast = false;
   return new Promise((resolve) => setTimeout(resolve, d * (fast ? 300 : 1000)));
 }
 async function fadeOutAndIn() {
